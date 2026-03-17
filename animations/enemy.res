363fcb27edd72fa30169f5e746f62f03431e71ce
m1k53r <mikobw369@protonmail.com>
2026-03-17 20:26:35 +0100

    wip

--- animations/enemy.res
+++ animations/enemy.res
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12a86690a1ba9fef1214dea2c116bd466d7698cc6ba593b1afdfd5d3b4ce2a00
-size 641
+oid sha256:b5cfe1a53c232363213be686c3ad0a7705cd2754a8d1f7fc6e286a6feae647cd
+size 673
--- scripts/actor.gd
+++ scripts/actor.gd
@@ -7,23 +7,39 @@ extends Node2D
 @export var hitmark_scene: PackedScene
 
 @onready var grid: Grid = find_parent("Grid")
-@onready var grid_animation_player: GridAnimationPlayer = find_children("", "GridAnimationPlayer")[0]
+# @onready var grid_animation_player: GridAnimationPlayer = find_children("", "GridAnimationPlayer")[0]
 @onready var status_bar: StatusBar = find_children("StatusBar", "HFlowContainer")[0]
+@onready var path_2d: Path2D = find_children("", "Path2D")[0]
+@onready var path_follow_2d: PathFollow2D = find_children("", "PathFollow2D")[0]
 
 var _audio_stream_player: AudioStreamPlayer
 
 var passive_status_effects: Array[StatusEffect] = []
 var active_status_effects: Array[StatusEffect] = []
+var is_moving: bool = false
 
 signal left_tile(tile: Vector2i)
 signal entered_tile(tile: Vector2i)
 signal health_changed
+signal finished_moving
 
 
 func _ready() -> void:
 	_audio_stream_player = AudioStreamPlayer.new()
 	_audio_stream_player.bus = sound_effect_bus
 	add_child(_audio_stream_player)
+
+
+func _process(delta: float) -> void:
+	if path_2d.curve.point_count:
+		path_follow_2d.progress_ratio += delta * 1
+		print(self.global_position)
+
+	if path_follow_2d.progress_ratio >= 0.9:
+		is_moving = false
+		path_2d.curve.clear_points()
+		finished_moving.emit()
+		set_process(false)
 
 
 func get_current_tile() -> Vector2i:
@@ -46,7 +62,8 @@ func execute(action: CombatAction) -> void:
 	if action is CombatAction.Move:
 		left_tile.emit(get_current_tile())
 		play_sound(move_sound)
-		await grid_animation_player.move_to(action.target_tile)
+		# await grid_animation_player.move_to(action.target_tile)
+		await execute_move(action.target_tile)
 		entered_tile.emit(action.target_tile)
 		return
 
@@ -76,9 +93,9 @@ func take_damage(value: int) -> void:
 	play_sound(hurt_sound, 0.1)
 	_show_hitmark(value)
 
-	if grid_animation_player.has_animation(&"hurt"):
-		health_changed.emit()
-		await grid_animation_player.play_and_wait(&"hurt")
+	health_changed.emit()
+	#if grid_animation_player.has_animation(&"hurt"):
+	#	await grid_animation_player.play_and_wait(&"hurt")
 
 
 func play_sound(sound: AudioStream, delay: float = 0.0) -> void:
@@ -125,3 +142,12 @@ func _show_hitmark(value: int):
 	var hitmark: Hitmark = hitmark_scene.duplicate().instantiate()
 	grid.add_child_on_tile(hitmark, self.get_current_tile())
 	hitmark.label.text = str(value)
+
+
+func execute_move(target_tile: Vector2i) -> void:
+	path_2d.curve.add_point(Vector2i.ZERO)
+	path_2d.curve.add_point(grid.map_to_local(target_tile) - grid.map_to_local(get_current_tile()))
+	print(path_2d.curve.get_point_position(1))
+	is_moving = true
+	set_process(true)
+	await finished_moving
