18271bd91fcc33e9c89629524d9abef4f5a4b61d
michalwa <michalwa2003@gmail.com>
2026-05-05 23:19:46 +0200

    fix audio stream node type & bus

--- scripts/status_effect_animation_player.gd
+++ scripts/status_effect_animation_player.gd
@@ -2,7 +2,6 @@ class_name StatusEffectAnimationPlayer
 extends Node2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var audio_stream: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func play_animation(animation_name: StringName) -> void:
 	animation_player.play(animation_name)
